7c13f1e4ffab1616c10141bf9955984406e405a3
Alex Reyes <alex.reyeswd@gmail.com>
2017-05-18 11:32:39 +0100

    ver pistas que te gustan

--- src/pages/likes/likes.ts
+++ src/pages/likes/likes.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DbApiService } from './../../shared/db-api.service';
+import { AuthService } from './../../providers/auth-service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-likes',
   templateUrl: 'likes.html'
 })
 export class LikesPage {
+  likes: any[];
+  tracks: any[];
+  tracks_filter: any[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private db: DbApiService, private auth: AuthService) {
 
+  }
+
+  ionViewDidLoad() {
+    this.db.getTracks().subscribe(resp => {
+      this.tracks = resp;
+      this.db.getLikes().subscribe(resp => {
+        this.likes = resp;
+        this.tracks_filter = [];
+        for (let track of this.likes) {
+          this.tracks_filter.push(_.find(this.tracks, (item) => {
+            return track.$key == item.$key;
+          }))
+        }
+      })
+    })
   }
 
 }
